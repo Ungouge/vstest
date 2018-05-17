fa04f1af1b8a80e24ff242028cecdd828637e4dd
Ungouge <zlobik89@gmail.com>
2018-05-17 20:42:25 +0200

    I coomit to this crime

--- testApp1/testApp1/Program.cs
+++ testApp1/testApp1/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace testApp1
 {
@@ -10,6 +6,7 @@ namespace testApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("silly, silly things")
         }
     }
 }
